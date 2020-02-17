ee5b661db1c980203533e760d15f140f01c0b7e7
Christoph Koch <koch@mpib-berlin.mpg.de>
2020-02-17 11:41:08 +0100

    Add interdependet model and fix mean bias

---
+++ code/simulation/PE_dep_LR_choice_model_interdep.R
@@ -0,0 +1,85 @@
+
+# Function to make a choice between two presented stimuli
+
+# - Updating of values only for chosen option
+# - Choice policy based on softmax
+
+PE_dep_LR_choice_model_interdep = function(design,
+                                           params.alpha0,
+                                           params.alpha1,
+                                           params.interdep,
+                                           params.temperature,
+                                           params.reward_space_ub,
+                                           choice_policy){
+  
+  # Get other parameters from design
+  # Number of trials
+  params.ntrials = nrow(design)
+  # Number of different stimuli with associated distributions
+  params.ndist = max(c(design$option_left, design$option_right))
+  
+  # Data frames to store choices, model values, prediction errors, and updating
+  df_choices = data.frame(matrix(NA, params.ntrials, 1))
+  # Initialize values with 50
+  df_values = data.frame(matrix(50, params.ntrials, params.ndist))
+  # Initialize prediction errors and updates with NA
+  df_pe = df_fpe = data.frame(matrix(NA, params.ntrials, params.ndist))
+  # Set colnames for each stimulus
+  colnames(df_values) = colnames(df_pe) = colnames(df_fpe) = paste('stim_',
+                                                                   as.character(c(1:params.ndist)),
+                                                                   sep='')
+  
+  # Loop over trials (leave out last trial to avoid updating beyond last trial)
+  for(trial_count in 1:(params.ntrials - 1)){
+    
+    # Get current comparison (choice options as 2 entry vector)
+    comp_stim = c(design$option_left[trial_count], design$option_right[trial_count])
+    comp_value = c(df_values[trial_count, comp_stim[1]], df_values[trial_count, comp_stim[2]])
+    comp_reward = c(design$reward_stim_1[trial_count], design$reward_stim_2[trial_count])
+    
+    # Select between softmax and greedy choice based on input
+    # Softmax
+    if(choice_policy == 'softmax'){
+      # Make softmax choice based on model values (returns index of choice in 2 entry vector)
+      choice = Softmax_choice(comp_value[1], comp_value[2], params.temperature)
+    # Greedy
+    } else if(choice_policy == 'greedy'){
+      # Make greedy choice
+      choice = which(comp_value == max(comp_value))
+      # In case of same value take random choice
+      if(length(choice) > 1){
+        choice = choice[sample(choice)[1]]
+      }
+    }
+    
+    # Get chosen stimulus, value, and reward
+    choice_stim = comp_stim[choice]
+    choice_value = comp_value[choice]
+    choice_reward = comp_reward[choice]
+    
+
+    
+    # Calculate updating acording to model
+    pe = choice_reward - choice_value
+    # dependency between alpha0 and alpha1 managed by extra parameter
+    fpe = params.interdep * params.alpha0 + (1 - params.interdep) * params.alpha1 * (abs(pe)/params.reward_space_ub)
+    updated_value = choice_value + fpe * pe
+    
+    # Update entries
+    # choice, pe, and fpe are updated in current trial
+    df_choices[trial_count, 1] = choice_stim
+    df_pe[trial_count, choice_stim] = pe
+    df_fpe[trial_count, choice_stim] = fpe
+    # Value is updated for all following trials
+    df_values[trial_count:nrow(df_values), choice_stim] = updated_value
+    
+  }
+  
+  # Form list of returns (and cut off extra line of model values after last choice)
+  model_data <- list('choices' = df_choices,
+                     'values' = df_values[1:params.ntrials,],
+                     'PE' = df_pe,
+                     'fPE' = df_fpe)
+  return(model_data)
+  
+}
