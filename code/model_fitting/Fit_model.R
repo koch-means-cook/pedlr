e4104565ba80b9d93e629c2c4a65ba63faf403d6
Christoph Koch <koch@mpib-berlin.mpg.de>
2021-11-04 17:10:24 +0100

    temporarily lower number of max evals during model fit

--- code/model_fitting/Fit_model.R
+++ code/model_fitting/Fit_model.R
@@ -32,9 +32,12 @@ Fit_model = function(data,
   # opts1 = list('algorithm'='NLOPT_GN_CRS2_LM',
   #              'xtol_rel'=1.0e-4,
   #              'maxeval'=500)
+  # opts1 = list('algorithm'='NLOPT_GN_DIRECT_L',
+  #              'xtol_rel'=1.0e-4,
+  #              'maxeval'= 5000)
   opts1 = list('algorithm'='NLOPT_GN_DIRECT_L',
                'xtol_rel'=1.0e-4,
-               'maxeval'= 5000)
+               'maxeval'= 1000)
   # Options for second (local) optimization
   opts2 = list('algorithm'='NLOPT_LN_COBYLA',
                'xtol_rel'=1.0e-4,
