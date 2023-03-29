b85a923aeca502d7a9291bfcc27e8c2b335b1982
Christoph Koch <koch@mpib-berlin.mpg.de>
2023-03-29 17:55:11 +0200

    Simulation runs

--- code/simulation/Simulation_wrapper.R
+++ code/simulation/Simulation_wrapper.R
@@ -1,3 +1,7 @@
+library(optparse)
+library(data.table)
+library(here)
+
 Simulation_wrapper = function(participant_id,
                               model,
                               x1_low,
@@ -14,23 +18,23 @@ Simulation_wrapper = function(participant_id,
                               x4_n,
                               temperature,
                               tau){
-
-    # participant_id = '09RI1ZH'
-    # model = 'surprise'
-    # x1_low = 0.1
-    # x1_high = 0.7
-    # x1_n = 5
-    # x2_low = 0.1
-    # x2_high = 0.7
-    # x2_n = 5
-    # x3_low = -10
-    # x3_high = 10
-    # x3_n = 5
-    # x4_low = NA
-    # x4_high = NA
-    # x4_n = NA
-    # tau = 0.2
-    # temperature = 7
+  
+  # participant_id = '09RI1ZH'
+  # model = 'surprise'
+  # x1_low = 0.1
+  # x1_high = 0.7
+  # x1_n = 5
+  # x2_low = 0.1
+  # x2_high = 0.7
+  # x2_n = 5
+  # x3_low = -10
+  # x3_high = 10
+  # x3_n = 5
+  # x4_low = NA
+  # x4_high = NA
+  # x4_n = NA
+  # tau = 0.2
+  # temperature = 7
   
   # Load own functions
   source(file.path(here::here(), 'code', 'simulation', 'Simulate.R', fsep = .Platform$file.sep))
@@ -70,7 +74,7 @@ Simulation_wrapper = function(participant_id,
     para_combs$x4 = NA
     # Specify parameter names
     para_names = c('l', 'u', 's')
-  } else if(model == 'uncertainty+surprise'){
+  } else if(model == 'uncertainty_surprise'){
     # 3 LRfunction parameters + pi (trailing surprise)
     x1 = seq(x1_low, x1_high, length.out = x1_n)
     x2 = seq(x2_low, x2_high, length.out = x2_n)
@@ -98,6 +102,10 @@ Simulation_wrapper = function(participant_id,
     # Get parameter combination of current iteration
     paras = as.matrix(para_combs[i,])[1,]
     
+    message('---- Parameter values ----')
+    message(paste(c('x1: ', 'x2: ', 'x3: ', 'x4: '), paras, '\n', sep = ''))
+    message('--------------------------')
+    
     # Simulate data based on parameter combination
     temp = Simulate(data = data,
                     x = paras,
@@ -108,9 +116,138 @@ Simulation_wrapper = function(participant_id,
   }
   
   # Save data in simulation out
-  file_pattern = file.path()
+  # Create save location if it does not exist yet
+  save_dir = file.path(here::here(), 'derivatives', 'simulation',
+                       fesp=.Platform$file.sep)
+  if(!file.exists(save_dir)){
+    dir.create(save_dir)
+  }
+  # Save output
+  file = file.path(save_dir, paste('modelsim_base-', participant_id,
+                                   '_model-', model, '.tsv', sep = ''),
+                   fsep = .Platform$file.sep)
+  data.table::fwrite(x = out, file = file, sep = '\t', na = 'n/a')
   
   # After: build HPC script
-
+  
   
 }
+
+# Function to split list inputs (used as callback function during argument parsing)
+split_list = function(object,
+                      flag,
+                      value,
+                      parser){
+  x = as.numeric(unlist(strsplit(value, ',')))
+  return(x)
+}
+
+# Create options to pass to script
+option_list = list(
+  optparse::make_option(c('-p', '--participant_id'),
+                        type='character',
+                        default = NULL,
+                        help = 'ID of participant',
+                        metavar = 'PARTICIPANT_ID'),
+  optparse::make_option(c('-m', '--model'),
+                        type='character',
+                        default = NULL,
+                        help = 'Specify model to use for simulation. "rw": x = c(alpha, NA, NA, NA); "uncertainty": x = c(alpha, pi, NA, NA); "surprise": x = c(l, u, s, NA); "uncertainty_surprise": x = c(l, u, s, NA)',
+                        metavar = 'MODEL'),
+  optparse::make_option(c('--x1_low'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Lower bound of grid for search over parameter 1',
+                        metavar = 'X1_LOW'),
+  optparse::make_option(c('--x1_high'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Upper bound of grid for search over parameter 1',
+                        metavar = 'X1_HIGH'),
+  optparse::make_option(c('--x1_n'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Number of equally spaced steps to form grid between lower and upper bound of parameter 1',
+                        metavar = 'X1_N'),
+  optparse::make_option(c('--x2_low'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Lower bound of grid for search over parameter 2',
+                        metavar = 'X2_LOW'),
+  optparse::make_option(c('--x2_high'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Upper bound of grid for search over parameter 2',
+                        metavar = 'X2_HIGH'),
+  optparse::make_option(c('--x2_n'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Number of equally spaced steps to form grid between lower and upper bound of parameter 2',
+                        metavar = 'X2_N'),
+  optparse::make_option(c('--x3_low'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Lower bound of grid for search over parameter 3',
+                        metavar = 'X3_LOW'),
+  optparse::make_option(c('--x3_high'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Upper bound of grid for search over parameter 3',
+                        metavar = 'X3_HIGH'),
+  optparse::make_option(c('--x3_n'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Number of equally spaced steps to form grid between lower and upper bound of parameter 3',
+                        metavar = 'X3_N'),
+  optparse::make_option(c('--x4_low'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Lower bound of grid for search over parameter 4',
+                        metavar = 'X4_LOW'),
+  optparse::make_option(c('--x4_high'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Upper bound of grid for search over parameter 4',
+                        metavar = 'X4_HIGH'),
+  optparse::make_option(c('--x4_n'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Number of equally spaced steps to form grid between lower and upper bound of parameter 4',
+                        metavar = 'X4_N'),
+  optparse::make_option(c('--temperature'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Temperature parameter for softmax function used in probabilistic choice during simulation',
+                        metavar = 'TEMPERATURE'),
+  optparse::make_option(c('--tau'),
+                        type='numeric',
+                        default = NULL,
+                        help = 'Parameter involved in LRfunction shape',
+                        metavar = 'TAU'))
+
+# provide options in list to be callable by script
+opt_parser = optparse::OptionParser(option_list = option_list)
+opt = optparse::parse_args(opt_parser)
+
+# Cal wrapper with command line inputs
+Simulation_wrapper(participant_id = opt$participant_id,
+                   model = opt$model,
+                   x1_low = opt$x1_low,
+                   x1_high = opt$x1_high,
+                   x1_n = opt$x1_n,
+                   x2_low = opt$x2_low,
+                   x2_high = opt$x2_high,
+                   x2_n = opt$x2_n,
+                   x3_low = opt$x3_low,
+                   x3_high = opt$x3_high,
+                   x3_n = opt$x3_n,
+                   x4_low = opt$x4_low,
+                   x4_high = opt$x4_high,
+                   x4_n = opt$x4_n,
+                   temperature = opt$temperature,
+                   tau = opt$tau)
+
+# Rscript Simulation_wrapper.R --participant_id '09RI1ZH' --model 'rw' --x1_low 0.1 --x1_high 0.7 --x1_n 5 --x2_low 0 --x2_high 0 --x2_n 0 --x3_low 0 --x3_high 0 --x3_n 0 --x4_low 0 --x4_high 0 --x4_n 0 --temperature 7 --tau 0.2
+# Rscript Simulation_wrapper.R --participant_id '09RI1ZH' --model 'uncertainty' --x1_low 0.1 --x1_high 0.7 --x1_n 5 --x2_low 0.1 --x2_high 0.7 --x2_n 5 --x3_low 0 --x3_high 0 --x3_n 0 --x4_low 0 --x4_high 0 --x4_n 0 --temperature 7 --tau 0.2
+# Rscript Simulation_wrapper.R --participant_id '09RI1ZH' --model 'surprise' --x1_low 0.1 --x1_high 0.7 --x1_n 5 --x2_low 0.1 --x2_high 0.7 --x2_n 5 --x3_low -10 --x3_high 10 --x3_n 5 --x4_low 0 --x4_high 0 --x4_n 0 --temperature 7 --tau 0.2
+# Rscript Simulation_wrapper.R --participant_id '09RI1ZH' --model 'uncertainty_surprise' --x1_low 0.1 --x1_high 0.7 --x1_n 5 --x2_low 0.1 --x2_high 0.7 --x2_n 5 --x3_low -10 --x3_high 10 --x3_n 5 --x4_low 0.1 --x4_high 0.7 --x4_n 5 --temperature 7 --tau 0.2
