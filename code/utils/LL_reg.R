f851252058f0639b8eb65aec687774dc211abba7
Christoph Koch <koch@mpib-berlin.mpg.de>
2023-09-04 18:24:04 +0200

    pseudocode for changes in LL calc

--- code/utils/LL_reg.R
+++ code/utils/LL_reg.R
@@ -16,6 +16,15 @@ LL_reg = function(x,
   preds = predict(cres$norm[[1]], type="response")
   probs = dbinom(cres$norm[[2]]$choice=='right', prob=preds, size=1, log=TRUE)
   cidx = which((cres$norm[[2]]$bandit == '12' | cres$norm[[2]]$bandit == '21'))
+  
+  # Ignore the first 3 updates of each bandit in the LL calculation (here large
+  # PEs arise which are not reflecting any surprise/rare outcomes because the
+  # paticipant has not built an expectation yet)
+  # GET INDEX FIRST 3 UPDATES OF CHOICE='1' IN [COMP='12' | COMP='21']
+  # SAME FOR CHOICE=='2'
+  # DELETE THOSE INDEXES FROM CIDX
+  
+  
   b2_logLik = -sum(probs[cidx])
   
   # Return negative LL of bandit comparison 1 vs 2
